151f96b7c2af62bd0c5bfae173e72d10f471eb0b
Tanwa Arpornthip <arpornthip@gmail.com>
2018-05-01 09:20:29 +0700

    Completed keepLogin, still have to figure out when the logout occurs

--- scripts/keepLogin/keepLogin.py
+++ scripts/keepLogin/keepLogin.py
@@ -1,9 +1,32 @@
 from splinter import Browser
+import datetime
+executable_path={'executable_path':'/usr/local/bin/chromedriver'}
+
+# Username and password. Clear text for now
+username = "YOUR USERNAME HERE"
+password = "YOUR PASSWORD HERE"
+logLocation = '/home/USER/log.csv'
+
+def logMeIn(browser):
+    browser.fill('username',username)
+    browser.fill('password',password)
+    browser.find_by_name('login')[1].click()
+    
 
 if __name__ == "__main__":
-    with Browser() as browser:
+    now = datetime.datetime.now()
+    nowString = now.strftime('%s'); # Using Epoch time
+    
+    with Browser('chrome', headless=True, incognito=True, **executable_path) as browser:
         # Visit URL
         url = "http://login.psu.ac.th"
+        resultString = nowString
         browser.visit(url)
-        if browser.is_text_present('This computer has been authenticated'):
-            print("Perfect. You're on.")
+        with open(logLocation,'a+') as logFile:
+            if browser.is_text_present('This computer has been authenticated'):
+                logFile.write(resultString+',True\n')
+            else:
+                logFile.write(resultString+',False\n')
+                logMeIn(browser)
+print("Connectivity status checked successfully at %s" %(now))
+            
